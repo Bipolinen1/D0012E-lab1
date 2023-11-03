6cb775033180df0c5a15647f8efb5a9ac8c23859
Bipolinen1 <hamben-2@student.ltu.se>
2023-11-03 12:35:45 +0100

    simplare

--- main.py
+++ main.py
@@ -12,3 +12,7 @@ startTime = time.time()
 print(iMergeSort(list1, 2))
 endTime = time.time()
 print(endTime-startTime)
+
+for i in range(2, 11):
+    list2 = [8, 7, 6, 5, 2, 9, 10, 12, -3, 78, -13, 3, 4, 5, 65]
+    print(bMergeSort(list2, i))
