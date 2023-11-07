b2d211ab121546dd63d2b3b33afb2bea941fe2c2
Hampus <hamben-2@student.ltu.se>
2023-11-07 13:12:14 +0100

    merge mergeSort and mergeSort2

--- main.py
+++ main.py
@@ -1,27 +1,14 @@
 from bMergeSort import *
 from iMergeSort import *
 from iterBMergeSort import *
-from bMergesort2 import *
 import time
 import random
 
 list1 = random.sample(range(-100000, 1000001), 100000)
-"""
-startTime = time.time()
-print(bMergeSort(list1, 2))
-endTime = time.time()
-print(endTime-startTime)
-startTime = time.time()
-print(iMergeSort(list1, 2))
-endTime = time.time()
-print(endTime-startTime)
 
-for i in range(2, 11):
-    list2 = [8, 7, 6, 5, 2, 9, 10, 12, -3, 78, -13, 3, 4, 5, 65]
-    print(bMergeSort(list2, i))"""
 
 startTime = time.time()
-print(bmerge_sort(list1, 4))
+print(bMergeSort(list1, 4))
 endTime = time.time()
 print(endTime-startTime)
 
