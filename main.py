96f0922d938e269370e238efb4d916d8f11b4000
Hampus <hamben-2@student.ltu.se>
2023-11-11 18:07:09 +0100

    testBmerge

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import random
+import time
+
 from bMergeSort import *
 from iMergeSort import *
 from iterBMergeSort import *
@@ -8,12 +11,19 @@ from simulateIterBMerge import *
 from simulateiMerge import *
 from simulateNormalMerge import *
 from simulateiMerge import *
+from testBmerge import  *
 """plotter(simulatebMerge(200, 200, 100000), range(1, 200), "bMerge")
 plotter(simulateIterBMerge(200, 200, 100000), range(1, 200), "iterBMerge")
 plotter(simulateiMerge(200, 200, 100000), range(1, 200), "iMerge")
 """
 
-nPlotter(simulateIncreasingNBMerge(), range(10000, 1010000, 100000), "bMerge")
+"""nPlotter(simulateIncreasingNBMerge(), range(10000, 1010000, 100000), "bMerge")
 nPlotter(simulateIncreasingNIterBMerge(), range(10000, 1010000, 100000), "iterBMerge")
 nPlotter(simulateIncreasingNIMerge(), range(10000, 1010000, 100000), "iMerge")
-nPlotter(simulateIncreasingNNormalMerge(), range(10000, 1010000, 100000), "nMerge")
+nPlotter(simulateIncreasingNNormalMerge(), range(10000, 1010000, 100000), "nMerge")"""
+
+ls = random.sample(range(1, 100001), 100000)
+start = time.time()
+print(hbMergeSort(ls, 3))
+end = time.time()
+print(end - start)
---
+++ testBmerge.py
@@ -0,0 +1,49 @@
+from bSort import bSort
+
+
+def hbMergeSort(ls, k):
+    if len(ls) <= k:
+        return bSort(ls)
+    if len(ls) % k != 0:
+        temp = len(ls) - (len(ls) % k)
+        if temp % 2 == 0:
+            split = temp // 2
+        else:
+            split = (temp + k) // 2
+    else:
+        if len(ls) % 2 == 0:
+            split = len(ls) // 2
+        else:
+            split = (len(ls) + k) // 2
+
+    ls1 = ls[:split]
+    ls2 = ls[split:]
+    l = hbMergeSort(ls1, k)
+    r = hbMergeSort(ls2, k)
+    i = 0
+    li = 0
+    ri = 0
+
+    while li < len(l) and ri < len(r):
+        if l[li] < r[ri]:
+            ls[i] = l[li]
+            li = li + 1
+        else:
+            ls[i] = r[ri]
+            ri = ri + 1
+        i = i + 1
+
+    if li == len(l):
+        for x in range(ri,len(r)):
+            ls[i] = r[x]
+            i = i + 1
+    elif ri == len(r):
+        for x in range(li,len(l)):
+            ls[i] = l[x]
+            i = i + 1
+
+    return ls
+
+
+
+
